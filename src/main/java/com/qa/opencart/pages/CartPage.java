04bbf72fe9600563145e827b41021775721fbbeb
Savita Sathe <savitas@ilink-systems.com>
2026-08-14 11:28:35 +0530

    updated the cart page as per review comment

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,11 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+	private By cartBtn=By.id("cart");
 
 	public CartPage() {
 		System.out.println("Cart page........");
 	}
 	public void addToCart() {
 		System.out.println("Add to cart...");
+		System.out.println("Add feature is done...");
 	}
 }
