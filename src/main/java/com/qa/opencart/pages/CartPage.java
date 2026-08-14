536e7d7a48ad92fd3f10fc47bfd1599f2543ba16
Savita Sathe <savitas@ilink-systems.com>
2026-08-14 10:49:33 +0530

    added cart page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+
+	public CartPage() {
+		System.out.println("Cart page........");
+	}
+	public void addToCart() {
+		System.out.println("Add to cart...");
+	}
+}
